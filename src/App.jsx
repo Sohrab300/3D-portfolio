95a22624b1d0b21d133dd14e13eb849266f275f7
Sohrab Sheikh <sheikhsohrab618@gmail.com>
2025-11-13 17:50:24 +0530

    Add Navbar, Showcase, and AnimatedCounter components; integrate react-countup for animations and smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Hero from './sections/Hero.jsx'
+import React from "react";
+import Hero from "./sections/Hero.jsx";
+import Showcase from "./sections/Showcase.jsx";
+import Navbar from "./components/Navbar.jsx";
+import LogoSection from "./sections/LogoSection.jsx";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Hero />
+      <Showcase />
+      <LogoSection />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/sections/LogoSection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LogoSection = () => {
+  return <div>LogoSection</div>;
+};
+
+export default LogoSection;
